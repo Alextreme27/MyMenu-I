e3d9ce3706c324e8df60b93bb365473926d9b561
Alexandre Barre <barre27@hotmail.com>
2019-09-18 17:04:59 +0200

    modification Order.java

--- src/com/ocr/anthony/Order.java
+++ src/com/ocr/anthony/Order.java
@@ -5,6 +5,7 @@ public class Order {
      * Display all available menus in the restaurant.
      */
     public void displayAvailableMenu() {
+        System.out.println("Choix menu \n 1 - Poulet \n 2 - Boeuf \n 3 - Végétarien \n Que souhaitez-vous comme menu?\n");
 
     }
     /**
@@ -12,6 +13,7 @@ public class Order {
      * @param nbMenu The selected menu.
      */
     public void displaySelectedMenu(int nbMenu) {
+        System.out.println("Vous avez choisi le menu " + nbMenu);
 
     }
 }
